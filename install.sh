7f820fffb6bddb1e8ec01bb3d0970fdac275697a
namkiba13 <89218132+namkiba13@users.noreply.github.com>
2022-12-11 11:00:01 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,11 +34,11 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth strong
+auth none
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
-$(awk -F "/" '{print "auth strong\n" \
+$(awk -F "/" '{print "auth none\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
